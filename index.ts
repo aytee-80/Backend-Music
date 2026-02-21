cf0eee12f7333641c509d7b5dd1391dfbb2bbe49
Amogelang Thomson <amogelangthompson@gmail.com>
2026-02-21 04:52:47 +0200

    index

--- index.ts
+++ index.ts
@@ -1,40 +1,8 @@
-import express from "express";
-import cors from "cors"; 
-import prisma from "./prisma/prismaClient";
+import app from "./src/app";
+import "dotenv/config";
 
-
-
-const app = express(); 
-app.use(cors()); 
-app.use(express.json()); 
-
-app.post("/users", async (req , res) => {
-    try{
-    const user = await prisma.user.create({
-        data: req.body
-    });
-    res.status(201).json(user);
-    }catch(error){
-        console.error(error);
-        res.status(500).json({error: "Failed to create user"});
-    }
-});
-
-app.get("/users", async (req, res) => {
-    try{
-    const users = await prisma.user.findMany(); 
-     res.json(users);
-    
-    }catch(error){
-        console.error(error);
-        res.status(500).json({error: "failed to fetch users"});
-    }
-});
-
-app.get("/",(req,res) => {
-    res.json({ message: "API is running"});
-})
+const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000"); 
+    console.log(`Server running on port 3000`); 
 });
