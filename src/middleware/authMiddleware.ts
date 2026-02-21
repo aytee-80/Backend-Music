666786df0b23744368d57dd0ed85d922a6f8d579
Amogelang Thomson <amogelangthompson@gmail.com>
2026-02-21 04:55:14 +0200

    authmiddleware

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,24 +1,34 @@
 import { Request , Response , NextFunction } from "express";
 import { firebaseAdmin } from "../config/firebase";
+import { DecodedIdToken } from "firebase-admin/auth";
 
-export async function authMiddleware(
-    req: Request,
-    res: Response,
-    next: NextFunction
-) {
-    const header = req.headers.authorization;
+export function authMiddleware(){
+    interface AuthRequest extends Request{
+        user?: DecodedIdToken;
+    }
+    const firebaseMiddleware = async(
+        req: AuthRequest,
+        res: Response,
+        next: NextFunction
+    ) => {
+        const header = req.headers.authorization;
 
-    if(!header?.startsWith("Bearer")){
-        return res.status(401).json({message: "Unauthorized"}); 
+        if(!header?.startsWith("Bearer")){
+            return res.status(401).json({message: "Unauthorized"}); 
+        }
+
+        const token = header.split(" ")[1]; 
+
+        try{
+            const decoded = await firebaseAdmin.auth().verifyIdToken(token);
+            req.user = decoded; 
+            next();
+        }catch{
+            res.status(401).json({message: "Invalid token"});
+        }
     }
 
-    const token = header.split(" ")[1]; 
-
-    try{
-        const decoded = await firebaseAdmin.auth().verifyIdToken(token);
-        req.user = decoded; 
-        next();
-    }catch{
-        res.status(401).json({message: "Invalid token"});
+    return{
+        firebaseMiddleware,
     }
 }
