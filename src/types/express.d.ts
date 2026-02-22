f5f79306f563046c7b5049ec9a0cef520cc89d71
Amogelang Thomson <amogelangthompson@gmail.com>
2026-02-22 23:31:19 +0200

    express

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,23 @@
 import { DecodedIdToken } from "firebase-admin/auth"
+import { Request } from "express";
 
-declare global{
-    namespace Express{
-        interface Request{
-            user?: DecodedIdToken
-        }
+// interface UserPayload{
+//     uid: string; 
+//     email: string;
+// }
+
+// declare global{
+//     namespace Express{
+//         interface Request{
+//             user?: DecodedIdToken 
+//         }
+//     }
+// }
+
+declare module 'express-serve-static-core'{
+    interface Request{
+        user?: DecodedIdToken;
     }
 }
+
+export {};
